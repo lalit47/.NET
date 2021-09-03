c53738288305427ec8640207dc63d3ebddebb6d1
LALIT KUMAR <Lalitpsc12@gmail.com>
2021-09-04 00:34:53 +0530

    Add files via upload

---
+++ DB_WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DB_wpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        DataSet ds;
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString= @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=JKDec20;Integrated Security=true";
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Employees";
+            
+            
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            ds=new DataSet();
+            da.Fill(ds,"Emps");
+
+            cmd.CommandText = "select * from Departments";
+            da.Fill(ds, "Depts");
+            dgEmps.ItemsSource = ds.Tables["Emps"].DefaultView;
+            cn.Close();
+        }
+    }
+}
