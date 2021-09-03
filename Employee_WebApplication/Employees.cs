4ba7aaf84ac7075696a337613d6bf6655ac18ce5
LALIT KUMAR <Lalitpsc12@gmail.com>
2021-09-04 00:53:19 +0530

    Add files via upload

---
+++ Employee_WebApplication/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee_WebApplication.Models
+{
+    public class Employees
+    {
+        private int EmpNo{get;set;}
+        private String Ename { get; set;}
+        private decimal Basic { get; set;}
+        private int DeptNo { get; set; }
+    }
+}
