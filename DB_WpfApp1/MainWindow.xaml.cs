bee6e6ab232e58f15e7f323c58882de25c021679
LALIT KUMAR <Lalitpsc12@gmail.com>
2021-09-04 00:32:32 +0530

    Add files via upload

---
+++ DB_WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DB_WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=JKDec20;Integrated Security=True";
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Employees";
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read()) {
+
+                lstBox.Items.Add(dr["Name"]);
+
+            }
+            dr.Close();
+            cn.Close();
+
+        }
+
+        private void btnNextResult_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source = (localdb)\MsSqlLocalDb; Initial Catalog = JKDec20; Integrated Security = True;";
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cn;
+            cmd.CommandText = "Select * from Employees;select * from Departments";
+            SqlDataReader dr = cmd.ExecuteReader();
+            lstBox.Items.Add("_______Emps__________");
+            while (dr.Read())
+            {
+                lstBox.Items.Add(dr["Name"]);
+
+            }
+            dr.NextResult();
+            lstBox.Items.Add("___________Depts_______");
+            while (dr.Read())
+            {
+                lstBox.Items.Add(dr["DeptName"]);
+
+            }
+            dr.Close();
+            cn.Close();
+        }
+
+        private void btnMultiResult_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source = (localdb)\MsSqlLocalDb; Initial Catalog = JKDec20; Integrated Security = True;MultipleActiveResultSets=True;";
+            cn.Open();
+            SqlCommand cmdDepts = new SqlCommand();
+            cmdDepts.Connection = cn;
+            cmdDepts.CommandType = CommandType.Text;
+            cmdDepts.CommandText = "Select * from Departments";
+
+            SqlCommand cmdEmps = new SqlCommand();
+            cmdEmps.Connection = cn;
+            cmdEmps.CommandType = CommandType.Text;
+
+            SqlDataReader drDepts = cmdDepts.ExecuteReader();
+            while (drDepts.Read())
+            {
+                lstBox.Items.Add(drDepts["DeptName"]);
+
+                cmdEmps.CommandText = "Select * from Employees where DeptNo = " + drDepts["DeptNo"];
+                SqlDataReader drEmps = cmdEmps.ExecuteReader();
+                while (drEmps.Read())
+                {
+                    lstBox.Items.Add("    " + drEmps["Name"]);
+                }
+                drEmps.Close();
+            }
+            drDepts.Close();
+            cn.Close();
+        }
+
+        private void btnGetResult_Click(object sender, RoutedEventArgs e)
+        {
+            
+            SqlDataReader dr = GetResult();
+            lstBox.Items.Add("Name");
+            while (dr.Read())
+            {
+                lstBox.Items.Add(dr["Name"]);
+            }
+        
+        }
+
+        private SqlDataReader  GetResult()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MsSqlLocalDb;Initial Catalog=JKDec20;Integrated Security=True";
+            cn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Employees";
+            SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            //dr.Close();
+            return dr;
+        }
+    }
+    }
