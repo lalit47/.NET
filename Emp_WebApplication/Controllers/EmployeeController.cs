c01c42d0f54e26d4eedc67cf86dc650ee0e3cb80
LALIT KUMAR <Lalitpsc12@gmail.com>
2021-09-04 00:41:10 +0530

    Add files via upload

---
+++ Emp_WebApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,105 @@
+﻿using Emp_WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Emp_WebApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmpDBContext db = new EmpDBContext();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employees = from e in db.Employees
+                            orderby e.ID
+                            select e;
+            return View(employees);
+           // return View();
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int EmpNo)
+        {
+          //  Employee objEmp = new Employee();
+            //objEmp.EmpNo = 123;
+            //objEmp.Name = "Vik";
+            //objEmp.Basic = 12345;
+            //objEmp.DeptNo = 10;
+               return View();
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+
+    internal class EmpDBContext
+    {
+        public object Employees { get; internal set; }
+    }
+}
---
+++ Emp_WebApplication/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Emp_WebApplication.Models
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public short DeptNo { get; set; }
+        public class EmpDBContext : DbContext
+        {
+            public EmpDBContext()
+            { }
+            public DbSet<Employee> Employees { get; set; }
+        }
+    }
+}
